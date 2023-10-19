19c3b2719b95f075d8dbd43aea2bef8cf89535dc
lowdon <franciscogalliscosta@live.com.pt>
2023-10-19 21:38:12 +0100

    Added hash message integrity verification for other AES encryption modes besides GCM

--- src/main/java/mchat/crypto/MessageEncoder.java
+++ src/main/java/mchat/crypto/MessageEncoder.java
@@ -11,11 +11,13 @@ import java.io.*;
 import java.security.*;
 import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
 // this class will hold all crypto algorithms able to be called
 public class MessageEncoder {
 
+    public static final String PROVIDER = "BC";
     //this will be the algorithm used for symmetric encryption. set it in the security.conf!!!
     private String confidentiality;
     private String cAlgorithm; //name of alg in confidentiality
@@ -42,12 +44,18 @@ public class MessageEncoder {
         keyBytes = confKeyS.getBytes();
 
         String[] confParts = confidentiality.split("/");
-        cAlgorithm = confParts[0];
-        cMode = confParts[1];
-        paddingType = confParts[2];
+        try { //verify integrity of conf file
+            if (confParts.length != 3)
+                throw new InvalidPropertiesFormatException("Invalid conf file setup");
+            cAlgorithm = confParts[0];
+            cMode = confParts[1];
+            paddingType = confParts[2];
+        } catch (InvalidPropertiesFormatException e){
+            throw new RuntimeException(e);
+        }
 
         try {
-            hash = MessageDigest.getInstance("SHA1", "BC"); // todo change to security.conf reading!!
+            hash = MessageDigest.getInstance("SHA1", PROVIDER); // todo change to security.conf reading!!
         } catch (NoSuchAlgorithmException | NoSuchProviderException e) {
             throw new RuntimeException(e);
         }
@@ -57,18 +65,13 @@ public class MessageEncoder {
 
         byte[] encryption = new byte[0];
 
-        //hash.update(input);
+        //todo must implement verification for prefered hashmode
+        hash.update(input);
 
         switch (this.cAlgorithm){
             case "AES":
                 encryption = encryptMessageWithAES(input);
                 break;
-            //case "AES/CCM/NoPadding":
-//                encryption = encryptMessageWithAESCCM(input);
-//                break;
-//            case "AES/CTR/NoPadding":
-//                encryption = encryptMessageWithAESCTR(input);
-//                break;
 //            case "DES/CBC/PKCS5Padding":
 //                encryption = encryptMessageWithDES(input);
 //                break;
@@ -95,13 +98,14 @@ public class MessageEncoder {
      */
     private byte[] encryptMessageWithAES(byte[] input) {
 
-        byte[] ivBytes;
-        AlgorithmParameterSpec ivSpec;
+        byte[] ivBytes = new byte[0];
+        AlgorithmParameterSpec ivSpec = null;
         SecretKeySpec key = new SecretKeySpec(this.keyBytes, cAlgorithm);
         Cipher cipher;
+        boolean needsIv = true;
 
         try {
-            cipher = Cipher.getInstance(confidentiality);
+            cipher = Cipher.getInstance(confidentiality, PROVIDER);
             switch (cMode) {
                 case "GCM":
                     ivBytes = generateIv(16);
@@ -115,11 +119,16 @@ public class MessageEncoder {
                     ivBytes = generateIv(13);
                     ivSpec = new IvParameterSpec(ivBytes);
                     break;
+                case "ECB":
+                    needsIv = false;
+                    break;
                 default:
                     throw new NoSuchAlgorithmException();
             }
         } catch (NoSuchAlgorithmException | NoSuchPaddingException e){
             throw new RuntimeException(e.getMessage());
+        } catch (NoSuchProviderException e) {
+            throw new RuntimeException(e);
         }
 
         System.out.println("key   : " + Utils.toHex(keyBytes));
@@ -129,13 +138,25 @@ public class MessageEncoder {
 
         // Cifrar
         try {
-            cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+            if(needsIv)
+                cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+            else // ecb mode
+                cipher.init(Cipher.ENCRYPT_MODE, key);
         } catch (InvalidKeyException e) {
             throw new RuntimeException(e);
         } catch (InvalidAlgorithmParameterException e) {
             throw new RuntimeException(e + " PROBLEM IN THE ENCRYPTION");
         }
-        byte[] cipherText = new byte[cipher.getOutputSize(input.length)];
+
+        //todo example for hash. there will be then hmac mac hash
+        boolean inHashMode = true;
+        byte[] cipherText = new byte[0];
+
+        if(cMode.equals("GCM"))
+            cipherText = new byte[cipher.getOutputSize(input.length)];
+        else if(inHashMode) //here we increase the buffer size to hold the hash
+            cipherText = new byte[cipher.getOutputSize(input.length + hash.getDigestLength() )];
+
         int ctLength = 0;
         try {
             ctLength = cipher.update(input, 0, input.length, cipherText, 0);
@@ -143,7 +164,13 @@ public class MessageEncoder {
             throw new RuntimeException(e);
         }
         try {
-            ctLength += cipher.doFinal(cipherText, ctLength);
+            if(cMode.equals("GCM")) //gcm does not need hash
+                ctLength += cipher.doFinal(cipherText, ctLength); // this is without hash in encrypted message
+            else {
+                ctLength += cipher.doFinal(hash.digest(), 0, hash.getDigestLength(), cipherText, ctLength);
+
+                System.out.println("message with hash " + hash.hashCode()); // todo debug
+            }
         } catch (IllegalBlockSizeException | ShortBufferException | BadPaddingException e) {
             throw new RuntimeException(e);
         }
@@ -156,13 +183,14 @@ public class MessageEncoder {
         try {
             dataStream.writeInt(ctLength); // int value in first pos
             dataStream.write(cipherText); // ciphertext afterwards
-            dataStream.write(ivBytes); // 16 for gcm or ctr and 13 bytes for ccm IV
+            if(needsIv)
+                dataStream.write(ivBytes); // 16 bytes for gcm or ctr and 13 bytes for ccm's IV
             dataStream.close();
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage()  + " PROBLEM IN THE ENCRYPTION IO EXEPT");
         }
 
-        System.out.println("message with hash " + Arrays.hashCode(input) + " encrypted"); // TODO DEBUG
+        System.out.println("encrypted successfully ---"); // TODO DEBUG
 
         return byteStream.toByteArray();
     }
@@ -171,12 +199,6 @@ public class MessageEncoder {
 //    }
 //
 //    private byte[] encryptMessageWithChaCha20(byte[] input) {
-//    }
-//
-//    private byte[] encryptMessageWithAESCTR(byte[] input) {
-//    }
-//
-//    private byte[] encryptMessageWithAESCCM(byte[] input) {
 //    }
 //
 //    private byte[] encryptMessageWithRC6(byte[] input) {
@@ -201,13 +223,14 @@ public class MessageEncoder {
                 } else { // we are in gcm or ctr mode
                     ivBytes = new byte[16];
                 }
-                istream.readFully(ivBytes);
+                if(!cMode.equals("ECB")) //if ecb there is no iv to read
+                    istream.readFully(ivBytes);
 
                 //define alg param spec
-                AlgorithmParameterSpec ivSpec;
+                AlgorithmParameterSpec ivSpec = null;
                 if(cMode.equals("GCM"))
                     ivSpec = new GCMParameterSpec(128, ivBytes);
-                else {
+                else if(cMode.equals("CCM") || cMode.equals("CTR")){
                     ivSpec = new IvParameterSpec(ivBytes);
                     //define hash for non auto-integrity algorithms
                     //hash.update(ctBytes); //todo tenho de ver onde posso colocar isto sem muita repeticao de codigo...
@@ -239,12 +262,6 @@ public class MessageEncoder {
 //    private byte[] decryptMessageWithChaCha20(byte[] input) {
 //    }
 //
-//    private byte[] decryptMessageWithAESCTR(byte[] input) {
-//    }
-//
-//    private byte[] decryptMessageWithAESCCM(byte[] input) {
-//    }
-//
 //    private byte[] decryptMessageWithRC6(byte[] input) {
 //    }
 //
@@ -266,9 +283,9 @@ public class MessageEncoder {
         DataOutputStream plainTextDataStream = new DataOutputStream(plainTextByteStream);
 
         try {
-            cipher = Cipher.getInstance(confidentiality);
+            cipher = Cipher.getInstance(confidentiality, PROVIDER);
 
-            System.out.println("ON DECRYPT: key   : " + Utils.toHex(keyBytes));
+            System.out.println("\nON DECRYPT: key   : " + Utils.toHex(keyBytes));
 
             System.out.println();
             System.out.println("input : " + Utils.toHex(cipherText));
@@ -278,7 +295,10 @@ public class MessageEncoder {
             if (ctLength == -1)
                 throw new ShortBufferException("Invalid ct length");
 
-            cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+            if(ivSpec == null) // ecb mode
+                cipher.init(Cipher.DECRYPT_MODE, key); // needs 18byte key
+            else
+                cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
 
             byte[] plainText = new byte[cipher.getOutputSize(ctLength)];
             int ptLength = 0;
@@ -286,17 +306,37 @@ public class MessageEncoder {
             ptLength = cipher.update(cipherText, 0, ctLength, plainText, 0);
             ptLength += cipher.doFinal(plainText, ptLength);
 
-            System.out.println("plain : " + Utils.toHex(plainText, ptLength) + " bytes: " + ptLength);
+            //message integrity verification with hash, will not be used for GCM or other autheticated modes
+            int messageLength = -1;
+            if(!cMode.equals("GCM")){
+                messageLength = ptLength - hash.getDigestLength();
 
-            plainTextDataStream.write(plainText); // ciphertext afterwards
+                hash.update(plainText, 0, messageLength);
+
+                byte[] messageHash = new byte[hash.getDigestLength()];
+                System.arraycopy(plainText, messageLength, messageHash, 0, messageHash.length);
+
+                //THIS WILL PRINT IF HASHES MATCH OR NOT!!!
+                System.out.println("plain : " + Utils.toString(plainText, messageLength) + "\nhashverified: " +
+                        MessageDigest.isEqual(hash.digest(), messageHash));
+
+                byte[] messageText = new byte[messageLength];
+                System.arraycopy(plainText, 0, messageText, 0, messageLength); // copies messageText from plain to msgText
+                plainTextDataStream.write(messageText);
+
+                System.out.println("message with hash " + hash.hashCode());
+            } else {
+                plainTextDataStream.write(plainText); // ciphertext afterwards
+                System.out.println("plain : " + Utils.toHex(plainText, ptLength) + " bytes: " + ptLength);
+            }
             plainTextDataStream.close();
 
-            System.out.println("message with hash " + Arrays.hashCode(plainText) + " decrypted ---"); // TODO DEBUG
-
+            //output
+            System.out.println("decrypted successfully ---");
 
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("our decrypted message is: " + Arrays.toString(plainTextByteStream.toByteArray()));
+            System.out.println("DECRYPT FAIL - our decrypted message is: " + Arrays.toString(plainTextByteStream.toByteArray()));
         }
 
         return plainTextByteStream.toByteArray();
